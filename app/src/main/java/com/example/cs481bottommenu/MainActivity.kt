2adeece366f3f7cd892625bc0186faa5121d681e
Anthony Herrera <juanherrera6192@gmail.com>
2024-07-30 23:14:59 -0400

    initial commmit

---
+++ app/src/main/java/com/example/cs481bottommenu/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.example.cs481bottommenu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomappbar.BottomAppBar
+import com.google.android.material.bottomnavigation.BottomNavigationItemView
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val homeFragment = HomeFragment()
+        val chartFragment = ChartFragment()
+        val settingsFragment = SettingsFragment()
+
+        changeFragment(homeFragment)
+
+        findViewById<BottomNavigationView>(R.id.bottom_nav).setOnItemSelectedListener { item ->
+            when(item.itemId) {
+                R.id.ic_home -> {
+                    changeFragment(homeFragment)
+                    true
+                }
+                R.id.ic_chart -> {
+                    changeFragment(chartFragment)
+                    true
+                } R.id.ic_settings -> {
+                    changeFragment(settingsFragment)
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+
+    private fun changeFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fContainer, fragment)
+            commit()
+        }
+    }
+}
